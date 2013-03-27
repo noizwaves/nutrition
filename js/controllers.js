01acf70b105aa244e6e7fb7c90879f6acb27dde7
Adam Neumann <adam@noizwaves.com>
2013-03-27 15:34:15 +1100

    Added list of basic nutrients in food items

--- js/controllers.js
+++ js/controllers.js
@@ -5,4 +5,15 @@
 angular.module('nutritionControllers', ['nutritionServices']).
     controller('HomeCtrl', ['$scope', 'Food', function($scope, Food) {
         $scope.food = Food.query();
+    }]).
+    controller('BasicFoodListCtrl', ['$scope', function($scope) {
+        $scope.basicNutrients = ['energy','protein','fat','carbohydrate'];
+    }]).
+    controller('FoodItemCtrl', ['$scope', 'Nutrient', function($scope, Nutrient) {
+        var food = $scope.f;
+        var nutrients = food['nutrition-per-100g'] || food['nutrition-per-100ml'] || {};
+
+        $scope.getAmount = function(nutrient) {
+            return nutrients[nutrient] || '-';
+        };
     }]);
