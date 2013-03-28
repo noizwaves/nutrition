5b9be64ff40e5f7f7f5b07314e9698ca2497bf9b
Adam Neumann <adam@noizwaves.com>
2013-03-28 17:49:01 +1100

    Finished UI for building temporary/in-memory meals with ingredients

--- js/controllers.js
+++ js/controllers.js
@@ -47,6 +47,50 @@ angular.module('nutritionControllers', ['nutritionServices']).
             });
         };
     }]).
-    controller('MealDetailList', ['$scope', function($scope) {
+    controller('MealIngredientList', ['$scope', 'Food', function($scope, Food) {
+        $scope.mode = 'list';
+        Food.query(function(foods) {
+            $scope.ingredients = [];
+            angular.forEach(foods, function(food) {
+                $scope.ingredients.push({
+                    food: food
+                });
+            });
+        });
 
+        $scope.toggle = function() {
+            var nextMode = angular.equals($scope.mode, 'list') ? 'edit' : 'list';
+
+            // TODO: do this more elegantly
+            if (angular.equals(nextMode, 'list')) {
+                $scope.meal.ingredients = [];
+                angular.forEach($scope.ingredients, function(ing) {
+                    if (angular.isNumber(ing.amount)) {
+                        $scope.meal.ingredients.push(ing);
+                    }
+                });
+            } else {
+                angular.forEach($scope.ingredients, function(i) {
+                    var inMeal = false;
+                    var amount;
+                    angular.forEach($scope.meal.ingredients, function(i2) {
+                        if (angular.equals(i, i2)) {
+                            amount = i2.amount;
+                            inMeal = true;
+                        }
+                    });
+                    if (inMeal) {
+                        i.amount = amount;
+                    } else {
+                        delete i.amount;
+                    }
+                });
+            }
+
+            $scope.mode  = nextMode;
+        };
+
+        $scope.getAmountUnit = function(food) {
+            return ('nutrition-per-100g' in food) ? 'g' : 'ml';
+        };
     }]);
