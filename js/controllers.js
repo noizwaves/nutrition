157460811a561424152768123895ec6d5914f08b
Adam Neumann <adam@noizwaves.com>
2013-03-28 16:10:10 +1100

    Showing units on food list

--- js/controllers.js
+++ js/controllers.js
@@ -6,14 +6,35 @@ angular.module('nutritionControllers', ['nutritionServices']).
     controller('HomeCtrl', ['$scope', 'Food', function($scope, Food) {
         $scope.food = Food.query();
     }]).
-    controller('BasicFoodListCtrl', ['$scope', function($scope) {
-        $scope.basicNutrients = ['energy','protein','fat','carbohydrate'];
+    controller('BasicFoodListCtrl', ['$scope', 'Nutrient', function($scope, Nutrient) {
+        // TODO: make these Nurtient resources
+        var basicNutrients = ['energy','protein','fat','carbohydrate'];
+
+        $scope.nutrients = Nutrient.query();
+
+        $scope.isBasic = function(n) {
+            var found = false;
+            angular.forEach(basicNutrients, function(bn) {
+                found |= angular.equals(bn, n.id);
+            });
+            return found;
+        };
     }]).
     controller('FoodItemCtrl', ['$scope', 'Nutrient', function($scope, Nutrient) {
         var food = $scope.f;
         var nutrients = food['nutrition-per-100g'] || food['nutrition-per-100ml'] || {};
 
         $scope.getAmount = function(nutrient) {
-            return nutrients[nutrient] || '-';
+            return nutrients[nutrient.id] || '-';
+        };
+
+        $scope.getUnit = function(nutrient) {
+            var specific = null;
+            angular.forEach(Nutrient.query(), function(d) {
+                if (d.id == nutrient) {
+                    specific = d;
+                }
+            });
+            return (specific || {}).unit;
         };
     }]);
